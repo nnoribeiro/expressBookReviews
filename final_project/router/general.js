7ab90d05706f15c971de6d9c172b13bd720933d4
Nuno <nno.ribeiro@gmail.com>
2024-05-07 04:40:49 -0400

    task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,32 +39,73 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+public_users.get('/', function (req, res) {
+    let getBooksPromise = new Promise((resolve, reject) => {
+        try {
+            resolve(books)
+        } catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+    return getBooksPromise
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({message: error}));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     let book = {};
-    Object.keys(books).forEach(k => k === isbn ? book = books[k] : [])
-    return res.status(200).json(book);
- });
+
+    let getBookPromise = new Promise((resolve, reject) => {
+        try {
+            Object.keys(books).forEach(k => k === isbn ? book = books[k] : [])
+            resolve(book)
+        } catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+    return getBookPromise
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({message: error}));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     let booksByAuthor = [];
-    Object.keys(books).forEach(k => books[k].author.toLowerCase() === author.toLowerCase() ? booksByAuthor.push({[k]: books[k]}) : [])
-    return res.status(200).json(booksByAuthor);
+    let getBookByAuthorPromise = new Promise((resolve, reject) => {
+        try {
+            Object.keys(books).forEach(k => books[k].author.toLowerCase() === author.toLowerCase() ? booksByAuthor.push({[k]: books[k]}) : [])
+            resolve(booksByAuthor)
+        } catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+    return getBookByAuthorPromise
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({message: error}));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let booksByTitle = [];
-    Object.keys(books).forEach(k => books[k].title.toLowerCase() === title.toLowerCase() || books[k].title.toLowerCase().includes(title.toLowerCase()) ? booksByTitle.push({[k]: books[k]}) : [])
-    return res.status(200).json(booksByTitle);
+    let getBookByTitlePromise = new Promise((resolve, reject) => {
+        try {
+            Object.keys(books).forEach(k => books[k].title.toLowerCase() === title.toLowerCase() || books[k].title.toLowerCase().includes(title.toLowerCase()) ? booksByTitle.push({[k]: books[k]}) : [])
+            resolve(booksByTitle)
+        } catch(error) {
+            console.log(error)
+            reject(error)
+        }
+    })
+    return getBookByTitlePromise
+        .then(result => res.status(200).json(result))
+        .catch(error => res.status(400).json({message: error}));
 });
 
 //  Get book review
